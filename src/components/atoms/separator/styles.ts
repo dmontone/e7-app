37790b0f4bc74df00df384f6ea3074e933737812
d m <dmontone@gmail.com>
2024-06-06 16:43:26 -0300

    creates separator component

---
+++ src/components/atoms/separator/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+export const Separator = styled.div`
+  width: 100%;
+  height: 1px;
+  background: ${({ theme }) => theme.colors.gray};
+  margin: 24px 0;
+`
