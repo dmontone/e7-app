17f560d4c7003e005c4c13e0e25178f2347374a3
d m <dmontone@gmail.com>
2024-06-06 13:48:50 -0300

    adds reset css

--- src/global.styles.ts
+++ src/global.styles.ts
@@ -1,6 +1,8 @@
 import { createGlobalStyle } from 'styled-components'
+import reset from 'styled-reset'
 
 export const GlobalStyles = createGlobalStyle`
+  ${reset}
   * {
     font-family: ${({ theme }) => theme.text.font};
   }
