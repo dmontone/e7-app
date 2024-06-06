7b2686533b12ea66d8a30feb9514796e990b97f7
d m <dmontone@gmail.com>
2024-06-06 16:18:50 -0300

    adds hero to header

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -1,6 +1,7 @@
 import { Button, Icon, Typography } from 'components/atoms'
 import { FC } from 'react'
 import * as S from './styles'
+import bgHero from './assets/bg-hero.jpg'
 
 type THeader = {
   title: string
@@ -11,10 +12,11 @@ export const Header: FC<THeader> = ({ title, text }) => {
   return (
     <S.Wrapper>
       <S.Hero>
+        <S.HeroImage img={bgHero} />
         <Typography variant='title' level={1} size={16} text={title} color={'white'} weight='bold' />
       </S.Hero>
       <Typography variant='text' size={8} text={text} />
-      
+
       <Button onClick={console.log}>
         vagas em aberto <Icon type='chevron-right' />
       </Button>
--- src/components/organisms/header/styles.ts
+++ src/components/organisms/header/styles.ts
@@ -7,8 +7,24 @@ export const Wrapper = styled.header`
 export const Hero = styled.div`
   width: 100%;
   height: 260px;
-  background: #F60;
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+  background: ${({ theme }) => theme.colors.black};
+
+  & > * { z-index: 2; }
+`
+
+export const HeroImage = styled.div<{ img: string }>`
+  width: 100%;
+  height: 100%;
+  background-image: url(${({ img }) => img});
+  background-size: cover;
+  background-position: center;
+  position: absolute;
+  top: 0;
+  left:0;
+  z-index: 1;
+  opacity: 0.7;
 `
