0d1a6b1ea667ce54bbb2ca2437dc855e83773f50
d m <dmontone@gmail.com>
2024-06-06 23:17:00 -0300

    updates photo component

--- src/components/atoms/photo/styles.tsx
+++ src/components/atoms/photo/styles.tsx
@@ -3,4 +3,13 @@ import styled from 'styled-components'
 export const Wrapper = styled.figure`
   border-radius: 16px;
   overflow: hidden;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    flex-shrink: 0;
+    min-width: 100%;
+    min-height: 100%;
+  }
 `
