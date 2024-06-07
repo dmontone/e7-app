38c83ad12297a1d7ac04985c2d6c10611c5f192c
d m <dmontone@gmail.com>
2024-06-07 16:18:31 -0300

    removes landing page props

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Icon, LandingPage } from 'components'
+import { LandingPage } from 'components'
 import { GlobalStyles } from 'global.styles'
 import { AppContextProvider } from 'state'
 import { ThemeProvider } from 'styled-components'
@@ -10,13 +10,7 @@ function App() {
       <ThemeProvider theme={Theme}>
         <>
           <GlobalStyles />
-          <LandingPage
-            header={{
-              title: 'trabalhe no elo7',
-              text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et viverra orci. Praesent consequat dolor tellus, eget viverra risus hendrerit non. Sed rutrum condimentum maximus. Donec pellentesque libero eu eros sagittis.',
-              cta: 'vagas em aberto'
-            }}
-          />
+          <LandingPage />
         </>
       </ThemeProvider>
     </AppContextProvider>
