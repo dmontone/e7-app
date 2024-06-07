d712991093ecb7784cd0c1396f10827cde7b902d
d m <dmontone@gmail.com>
2024-06-07 16:20:09 -0300

    clear unused data from landing page template

--- src/components/templates/landing-page/landing-page.tsx
+++ src/components/templates/landing-page/landing-page.tsx
@@ -1,40 +1,22 @@
 import { FC } from 'react'
 import * as S from './styles'
-import { Typography } from 'components/atoms'
-import { Header } from 'components/organisms'
+import { Header, Team } from 'components/organisms'
 import { Testimonial } from 'components/organisms/testimonial/testimonial'
 
-type TLandingPage = {
-  header: {
-    title: string,
-    text: string,
-    cta: string,
-  }
-}
-
-export const LandingPage: FC<TLandingPage> = ({ header }) => {
+export const LandingPage: FC = () => {
   return (
     <S.Wrapper>
-      <Header {...header} />
+      <Header ctaOnClick={console.log} />
       <main>
         <Testimonial />
-
-        Section: Team
-        <section>
-          <Typography variant='title' level={2} size={14} weight='semibold' text={'Conheça nosso time'} />
-          {/* Figure */}
-          {/* Section: Features */}
-          <section>
-            {/* Cards */}
-          </section>
-        </section>
+        <Team />
 
         {/* <div> Figure </div> */}
 
         {/* Section: Jobs */}
-        <section>
+        {/* <section>
           <Typography variant='title' level={2} size={16} weight='semibold' text={'Vagas em aberto'} />
-        </section>
+        </section> */}
       </main>
     </S.Wrapper>
   )
