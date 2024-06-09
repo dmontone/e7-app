4b5bea36490215fb8c3ff568b75a80ccf39876a8
d m <dmontone@gmail.com>
2024-06-09 12:06:09 -0300

    creates search molecule

--- src/components/molecules/index.ts
+++ src/components/molecules/index.ts
@@ -1,2 +1,3 @@
 export * from './card'
 export * from './member-list'
+export * from './search'
---
+++ src/components/molecules/search/search.tsx
@@ -0,0 +1,10 @@
+import { Input } from 'components/atoms'
+import { FC } from 'react'
+
+export const Search: FC = () => {
+  return (
+    <>
+      <Input placeholder='nome da vaga' />
+    </>
+  )
+}
