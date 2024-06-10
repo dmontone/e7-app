489fa6030197a1199becdfc367b0a6edd7aae4d7
d m <dmontone@gmail.com>
2024-06-10 12:59:57 -0300

    adds jobs section to landing page

--- src/components/templates/landing-page/landing-page.tsx
+++ src/components/templates/landing-page/landing-page.tsx
@@ -1,9 +1,10 @@
-import { FC } from 'react'
+import { FC, useEffect } from 'react'
 import * as S from './styles'
 import { Features, Header, Jobs, Team, Testimonial } from 'components/organisms'
 import { Filler } from 'components/atoms'
 
 export const LandingPage: FC = () => {
+
   return (
     <S.Wrapper>
       <Header ctaOnClick={console.log} />
