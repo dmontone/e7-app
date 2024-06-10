589d93f9c39882f962f9ef9c61482722fb991bb8
d m <dmontone@gmail.com>
2024-06-10 10:41:16 -0300

    adds size property to icon component

--- src/components/atoms/icon/icon.tsx
+++ src/components/atoms/icon/icon.tsx
@@ -1,16 +1,17 @@
 import { FC } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faChevronRight, faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faChevronRight, faSearch, faSpinner } from '@fortawesome/free-solid-svg-icons'
 
-const icons = [faChevronRight, faSearch]
+const icons = [faChevronRight, faSearch, faSpinner]
 const iconNames = icons.map(icon => icon.iconName)
 library.add(...icons)
 
 type TIcon = {
   type: typeof iconNames[number]
+  size?: 1 | 2 | 3 | 4 | 5
 }
 
-export const Icon: FC<TIcon> = ({ type }) => {
-  return <FontAwesomeIcon icon={type} />
+export const Icon: FC<TIcon> = ({ type, size = 1 }) => {
+  return <FontAwesomeIcon icon={type} size={`${size}x`} />
 }
