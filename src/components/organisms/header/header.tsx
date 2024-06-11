aca50622cc8ff64cfaf188965c0d32e314af43a6
d m <dmontone@gmail.com>
2024-06-11 13:14:58 -0300

    adds responsive version to header organism

--- src/components/organisms/header/header.tsx
+++ src/components/organisms/header/header.tsx
@@ -1,9 +1,8 @@
 import { Button, Icon, Separator, Typography } from 'components/atoms'
 import { FC, MouseEvent, useState } from 'react'
+import { LazyLoadImage } from 'react-lazy-load-image-component'
 import * as S from './styles'
 import bgHero from './assets/bg-hero.jpg'
-import { LazyLoadImage } from 'react-lazy-load-image-component'
-import 'react-lazy-load-image-component/src/effects/opacity.css'
 
 type THeader = {
   ctaOnClick: (e: MouseEvent) => void
@@ -18,10 +17,10 @@ export const Header: FC<THeader> = ({ ctaOnClick }) => {
         <S.HeroImage $isVisible={isImageLoaded}>
           <LazyLoadImage src={bgHero} alt={'Lorem ipsum'} onLoad={() => setImageLoaded(true)} />
         </S.HeroImage>
-        <Typography variant='title' level={1} size={32} color={'white'} weight='bold' text={'trabalhe no elo7'} />
+        <Typography variant='title' level={1} size={[32, 56, 80]} color={'white'} weight='bold' text={'trabalhe no elo7'} />
       </S.Hero>
       <S.Content>
-        <Typography variant='text' size={16} lineHeight={1.5} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et viverra orci. Praesent consequat dolor tellus, eget viverra risus hendrerit non. Sed rutrum condimentum maximus. Donec pellentesque libero eu eros sagittis.'} />
+        <Typography variant='text' size={[16, 20, 24]} lineHeight={[24, 28, 32]} text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et viverra orci. Praesent consequat dolor tellus, eget viverra risus hendrerit non. Sed rutrum condimentum maximus. Donec pellentesque libero eu eros sagittis.'} />
         <Separator />
         <Button onClick={ctaOnClick}>
           vagas em aberto <Icon type='chevron-right' />
--- src/components/organisms/header/styles.ts
+++ src/components/organisms/header/styles.ts
@@ -1,6 +1,9 @@
+import { device } from 'device.config'
 import styled from 'styled-components'
 
-export const Wrapper = styled.header``
+export const Wrapper = styled.header`
+  height: auto;
+`
 
 export const Hero = styled.div(() => ({
   width: '100%',
@@ -10,7 +13,17 @@ export const Hero = styled.div(() => ({
   justifyContent: 'center',
   position: 'relative',
 
-  ['& > *']: { zIndex: 2 }
+  [`@media ${device.tablet}`]: {
+    height: 380
+  },
+
+  [`@media ${device.desktop}`]: {
+    height: 500
+  },
+
+  ['& > *']: {
+    zIndex: 2
+  }
 }))
 
 export const HeroImage = styled.div<{ $isVisible: boolean }>(({ theme, $isVisible }) => ({
@@ -22,7 +35,7 @@ export const HeroImage = styled.div<{ $isVisible: boolean }>(({ theme, $isVisibl
   height: 260,
   background: theme.colors.black,
   opacity: +$isVisible,
-  transition: 'opacity 1s ease-out',
+
   img: {
     width: '100%',
     height: '100%',
@@ -30,10 +43,26 @@ export const HeroImage = styled.div<{ $isVisible: boolean }>(({ theme, $isVisibl
     objectPosition: 'center',
     display: 'block',
     opacity: 0.7,
-  }
+  },
+
+  [`@media ${device.tablet}`]: {
+    height: 380
+  },
+
+  [`@media ${device.desktop}`]: {
+    height: 500
+  },
 }))
 
 export const Content = styled.div(() => ({
   padding: '32px 16px 40px 16px',
   textAlign: 'center',
+
+  [`@media ${device.tablet}`]: {
+    margin: '0 12px'
+  },
+
+  [`@media ${device.desktop}`]: {
+    margin: '0 40px'
+  },
 }))
